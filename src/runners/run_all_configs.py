a423e8e215d5d1a121ca3e630eca4414b8a04c69
Alain Joss <alain.joss.1998@gmail.com>
2026-05-11 13:04:41 +0200

    Corrected runner.

--- src/runners/run_all_configs.py
+++ src/runners/run_all_configs.py
@@ -1,5 +1,6 @@
 import argparse
 from copy import deepcopy
+from itertools import product
 from typing import Any
 
 from src.paths import CONFIGS
@@ -11,17 +12,23 @@ from src.utils import (
     ensure_rollout_dir,
     get_device,
     update_experiment_params,
-    read_config, read_json, list_tens_to_floats
+    read_json,
+    list_tens_to_floats,
 )
+
+
+ALPHAS = [2.0]
+WS = [5.0, 10.0, 15.0]
 
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     parser.add_argument("--test", action="store_true")
-    parser.add_argument("--config-type", choices=["guided", "unguided"], required=True)
-    parser.add_argument("--guidance-mode", choices=GUIDANCE_MODES)
-    parser.add_argument("--alpha", type=float)
-    parser.add_argument("--w", type=float)
+    parser.add_argument(
+        "--rollout-type",
+        choices=["guided", "unguided"],
+        required=True,
+    )
     return parser.parse_args()
 
 
@@ -63,38 +70,53 @@ def apply_overrides(
     return config
 
 
-def main():
+def main() -> None:
+    args = parse_args()
+
     device = get_device()
     flow_model = get_model(device)
 
-    args = parse_args()
-
-    configs = load_configs(args.config_type)
+    configs = load_configs(args.rollout_type)
     print(f"found {len(configs)} configs")
 
     for idx, (config_id, config) in enumerate(configs, start=1):
         print(f"running config {idx}/{len(configs)}: {config_id}")
 
         rollout_dir = ensure_rollout_dir(config["rollout_id"])
-    
-        config = apply_overrides(
-            config,
-            guidance_mode=args.guidance_mode,
-            alpha=args.alpha,
-            w=args.w,
-        )
-    
-        if config["guidance_flag"]:
-            update_experiment_params(rollout_dir, config)
 
-        rollout_dir = rollout_from_config(
-            flow_model,
-            rollout_dir,
-            config,
-            test=args.test,
-        )
+        if args.rollout_type == "guided":
+            for guidance_mode, alpha, w in product(GUIDANCE_MODES, ALPHAS, WS):
+                config_ = apply_overrides(
+                    config,
+                    guidance_mode=guidance_mode,
+                    alpha=alpha,
+                    w=w,
+                )
 
-        print(f"saved rollout to: {rollout_dir}")
+                update_experiment_params(rollout_dir, config_)
+
+                rollout_dir = rollout_from_config(
+                    flow_model,
+                    rollout_dir,
+                    config_,
+                    test=args.test,
+                )
+
+                print(
+                    "saved rollout to: "
+                    f"{rollout_dir} | "
+                    f"mode={guidance_mode}, alpha={alpha}, w={w}"
+                )
+
+        else:
+            rollout_dir = rollout_from_config(
+                flow_model,
+                rollout_dir,
+                config,
+                test=args.test,
+            )
+
+            print(f"saved rollout to: {rollout_dir}")
 
 
 if __name__ == "__main__":
